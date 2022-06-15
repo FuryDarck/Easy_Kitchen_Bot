1d0024698e27f192fbfa0d9c366a2f5e3801dd2c
GhostForge <rustambqwe@gmail.com>
2022-06-15 19:18:00 +0300

    Добавление триггера

--- SQL Server/Trigger_Kitch_1.sql
+++ SQL Server/Trigger_Kitch_1.sql
@@ -5,15 +5,32 @@ As
 Begin
 Declare @Us NVARCHAR(30)
 Declare @d int
---Set @d = 953
-Set @d = 953 --����� �� ��, ��� ���� ������
 Set @Us = (Select ingredient_name From inserted)
-
-
+If Exists((Select * From Ingredients Where ingredient_name =(Select @Us From inserted)))
+Begin
+Select id_ingredients From Ingredients
+Where ingredient_name =(Select @Us From inserted)
+End
 Else
 Begin
-
 Select id_ingredients From Ingredients where id_ingredients = 0
+End
+End
 
+--Триггер на поиск блюда из Buffer_Id
+Drop Trigger Buffer_IDs
+
+Create Trigger Buffer_IDs
+on dbo.Buffer_Id
+Instead Of Insert
+As
+Begin
+Declare @id_buf int
+Declare @id_ingred int
+Set @id_ingred = (Select id_ingredients From inserted)
+If Exists((Select * From Buffer_Id Where id_ingredients = (Select @id_ingred From inserted)))
+Begin
+Select id_food From Buffer_Id
+Where id_ingredients = (Select @id_ingred From inserted)
 End
 End
